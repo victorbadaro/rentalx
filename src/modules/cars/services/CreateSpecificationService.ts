64e9795332f5f1ba07cddca44c33502f6f309bdd
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2023-08-29 19:16:43 -0300

    feat: add service, repository, interface and routes files for the specifications resource

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,26 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute() {
-    console.log();
+  private specificationRepository: ISpecificationsRepository;
+
+  constructor(specificationRepository: ISpecificationsRepository) {
+    this.specificationRepository = specificationRepository;
+  }
+
+  execute({ name, description }: IRequest): void {
+    const specificationAlreadyExists =
+      this.specificationRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error("Specification already exists!");
+    }
+
+    this.specificationRepository.create({ name, description });
   }
 }
 
