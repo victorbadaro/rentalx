1db6ceb3341cb5a34b32195d9dd3c761ba602c85
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2023-10-16 18:18:13 -0300

    feat: improve error handling

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 import "reflect-metadata";
 import swaggerUI from "swagger-ui-express";
 
+import { AppError } from "./errors/AppError";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
@@ -13,5 +15,19 @@ const app = express();
 app.use(express.json());
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 app.use(router);
+
+app.use(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({ message: err.message });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`
+    });
+  }
+);
 
 app.listen(3333, () => console.log("Server is running"));
