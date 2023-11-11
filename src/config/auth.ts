ce674aef4a4e923de9ac31e7b53759e5ccc00567
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2023-11-11 00:49:47 -0300

    feat: add refresh token authentication method

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "5f90046bbb4f2f7e9f2333d15fd0389b",
+  expires_in_token: "15m",
+  secret_refresh_token: "7faa6d42a80420234f3d434ed0f49307",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30
+};
--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,7 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 
-import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+import auth from "@config/auth";
+import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
 import { AppError } from "@shared/errors/AppError";
 
 interface IPayload {
@@ -14,6 +15,8 @@ export async function ensureAuthenticated(
   next: NextFunction
 ) {
   const authHeader = request.headers.authorization;
+  const { secret_refresh_token } = auth;
+  const usersTokensRepository = new UsersTokensRepository();
 
   if (!authHeader) {
     throw new AppError("Token missing", 401);
@@ -22,19 +25,18 @@ export async function ensureAuthenticated(
   const [, token] = authHeader.split(" ");
 
   try {
-    const { sub: user_id } = verify(
-      token,
-      "5f90046bbb4f2f7e9f2333d15fd0389b"
-    ) as IPayload;
-    const usersRepository = new UsersRepository();
-    const user = await usersRepository.findById(user_id);
+    const { sub: user_id } = verify(token, secret_refresh_token) as IPayload;
+    const user = await usersTokensRepository.findByUserIdAndRefreshToken(
+      user_id,
+      token
+    );
 
     if (!user) {
       throw new AppError("User does not exists!", 401);
     }
 
     request.user = {
-      id: user.id
+      id: user_id
     };
 
     next();
