960aa788b7eedbbe4afc5666c8080314d9641aa4
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2023-11-18 23:36:17 -0300

    feat: add endpoint to get information from a specific user

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -4,10 +4,12 @@ import "express-async-errors";
 import "reflect-metadata";
 import swaggerUI from "swagger-ui-express";
 
+import upload from "@config/upload";
 import { AppError } from "@shared/errors/AppError";
 import createConnection from "@shared/infra/typeorm";
 
 import "@shared/container";
+
 import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
 
@@ -17,6 +19,8 @@ const app = express();
 
 app.use(express.json());
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
+app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
+app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 app.use(router);
 
 app.use(
