b0a647a9e398630ab3adb2a52bcda70bcdd95ee9
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2023-12-01 01:59:30 -0300

    feat: add cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
@@ -21,6 +22,7 @@ app.use(express.json());
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
+app.use(cors());
 app.use(router);
 
 app.use(
